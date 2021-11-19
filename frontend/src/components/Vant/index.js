e447e4e4b8ced449cba3279984dff0fe801c66c6
Lobfly <2849208927@qq.com>
2021-11-19 15:56:11 +0800

    fix: register vant component

--- frontend/src/components/Vant/index.js
+++ frontend/src/components/Vant/index.js
@@ -1,18 +1,30 @@
-import Vue from "vue";
+import Vue from 'vue'
 import {
-  Button,
-  Tabbar,
-  TabbarItem,
-  Uploader, 
-  Icon, 
-  Skeleton,
-  List
-} from "vant";
+	Button,
+	Tabbar,
+	TabbarItem,
+	Uploader,
+	Icon,
+	Skeleton,
+	List,
+	Cell,
+	CellGroup,
+	Field,
+	Col,
+	Row,
+	Checkbox,
+} from 'vant'
 
 Vue.use(Button)
-  .use(Tabbar)
-  .use(TabbarItem)
-  .use(Uploader)
-  .use(Icon)
-  .use(Skeleton)
-  .use(List)
+	.use(Tabbar)
+	.use(TabbarItem)
+	.use(Uploader)
+	.use(Icon)
+	.use(Skeleton)
+	.use(List)
+	.use(Cell)
+	.use(CellGroup)
+	.use(Field)
+	.use(Col)
+	.use(Row)
+	.use(Checkbox)
